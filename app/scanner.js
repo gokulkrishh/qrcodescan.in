00c6f7bbe759c30f9d81d7b0530ef1e2fcdc55b4
gokulkrishh <krishnangokul9@gmail.com>
2016-03-16 12:51:55 +0530

    Added qr scanner lib

--- app/scanner.js
+++ app/scanner.js
@@ -1,39 +1,39 @@
-var Quagga = require('quagga');
-
-var btn = document.querySelector('.custom-btn');
-
-btn.addEventListener('click', function () {
-  accessCamera();
-});
-
-function accessCamera() {
-  Quagga.init(
-    {
-      inputStream: {
-        type : "LiveStream",
-        target: document.querySelector('#qr-code'),
-        constraints: {
-          width: 640,
-          height: 480,
-          facing: "environment" // or user
-        }
-      },
-      locator: {
-        patchSize: "medium",
-        halfSample: true
-      },
-      numOfWorkers: 4,
-      decoder: {
-        readers : ["code_128_reader"]
-      },
-      locate: true
-    },
-    function(err) {
-      if (err) {
-        console.log(err);
-        return
-      }
-      alert('Initialization finished. Ready to start');
-      // Quagga.start();
-  });
-}
+// var Quagga = require('quagga');
+//
+// var btn = document.querySelector('.custom-btn');
+//
+// btn.addEventListener('click', function () {
+//   accessCamera();
+// });
+//
+// function accessCamera() {
+//   Quagga.init(
+//     {
+//       inputStream: {
+//         type : "LiveStream",
+//         target: document.querySelector('#qr-code'),
+//         constraints: {
+//           width: 640,
+//           height: 480,
+//           facing: "environment" // or user
+//         }
+//       },
+//       locator: {
+//         patchSize: "medium",
+//         halfSample: true
+//       },
+//       numOfWorkers: 4,
+//       decoder: {
+//         readers : ["code_128_reader"]
+//       },
+//       locate: true
+//     },
+//     function(err) {
+//       if (err) {
+//         console.log(err);
+//         return
+//       }
+//       alert('Initialization finished. Ready to start');
+//       // Quagga.start();
+//   });
+// }
