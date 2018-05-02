7957297e21097f0bf51872266c699fdf955f02e4
Yevhen Kochuh <yevhen.kochuh@yevhenkchuhsmbp.do.local>
2018-05-02 12:44:46 +0300

    fixed deprecated babel-preset-es2015 dependency

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ const config = {
         exclude: /node_modules/,
         use: [{
           loader: 'babel-loader',
-          options: { presets: ['es2015'] }
+          options: { presets: ['env'] }
         }]
       },
       {
