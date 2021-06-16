14249016564da9fe2c07178a54fe011d20021453
gokulkrishh <krishnangokul9@gmail.com>
2021-06-16 09:20:17 +0530

    Introduce rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,20 @@
+import styles from "rollup-plugin-styles";
+import { terser } from "rollup-plugin-terser";
+const { generateSW } = require("rollup-plugin-workbox");
+
 export default {
-  input: 'src/js/main.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs'
-  }
+	input: "src/js/main.js",
+	output: {
+		file: "public/bundle.js",
+		format: "cjs",
+		assetFileNames: "[name]-[hash][extname]"
+	},
+	plugins: [
+		styles(),
+		terser(),
+		generateSW({
+			swDest: "/public/sw.js",
+			globDirectory: "public/sw/"
+		})
+	]
 };
--- src/js/snackbar.js
+++ src/js/snackbar.js
@@ -1,24 +1,24 @@
 var snackbar = {};
-var snackBarElement = document.querySelector('.app__snackbar');
+var snackBarElement = document.querySelector(".app__snackbar");
 var snackbarMsg = null;
 
 //To show notification
 snackbar.show = (msg, options = 4000) => {
-  if (!msg) return;
+	if (!msg) return;
 
-  if (snackbarMsg) {
-    snackbarMsg.remove();
-  }
+	if (snackbarMsg) {
+		snackbarMsg.remove();
+	}
 
-  snackbarMsg = document.createElement('div');
-  snackbarMsg.className = 'app__snackbar-msg';
-  snackbarMsg.textContent = msg;
-  snackBarElement.appendChild(snackbarMsg);
+	snackbarMsg = document.createElement("div");
+	snackbarMsg.className = "app__snackbar-msg";
+	snackbarMsg.textContent = msg;
+	snackBarElement.appendChild(snackbarMsg);
 
-  //Show toast for 3secs and hide it
-  setTimeout(() => {
-    snackbarMsg.remove();
-  }, options);
+	//Show toast for 3secs and hide it
+	setTimeout(() => {
+		snackbarMsg.remove();
+	}, options);
 };
 
-exports.snackbar = snackbar;
+export default snackbar;
